5ef502d52af37bb4cc1c1367acb624cdf45d290c
Leandro <LeandroSilvaDeOliveira0605@gmail.com>
2025-07-11 13:07:09 -0300

    db settings fix

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'drizzle-kit';
+import { env } from './src/env.ts';
+
+export default defineConfig({
+  dialect: 'postgresql',
+  casing: 'snake_case',
+  schema: './src/db/schema/**.ts',
+  out: './src/db/migration',
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+});
+
+// npx drizzle-kit generate
